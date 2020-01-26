7f2dd3ad33da55a831b5516dcfe9cf020d205380
Andrew Jones <aej11@hotmail.com>
2020-01-26 01:15:55 -0500

    Updates

---
+++ precache-manifest.486abd6aff923a275f3f2d8ea3004cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b880203d8dea6021edf7250378b344f",
+    "url": "/react-userbase/index.html"
+  },
+  {
+    "revision": "7e4943daf2fb43eb60aa",
+    "url": "/react-userbase/static/css/main.52871052.chunk.css"
+  },
+  {
+    "revision": "634423fa279f9a8c54e1",
+    "url": "/react-userbase/static/js/2.b53cf686.chunk.js"
+  },
+  {
+    "revision": "2fe9a55bda9864aebc0b21cc3eb75f88",
+    "url": "/react-userbase/static/js/2.b53cf686.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e4943daf2fb43eb60aa",
+    "url": "/react-userbase/static/js/main.53753a9c.chunk.js"
+  },
+  {
+    "revision": "c49781293166852feba4",
+    "url": "/react-userbase/static/js/runtime-main.7860ffd6.js"
+  }
+]);
